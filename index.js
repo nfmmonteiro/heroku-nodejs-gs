0624c51550a66470edf286266fb68a3105c94230
Nuno Monteiro <nfmmonteiro@gmail.com>
2016-02-20 23:07:26 +0000

    Adds /db endpoint that connects to a postgreSQL database to display rows from a table

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-var cool = require('cool-ascii-faces');
+var cool 		= require('cool-ascii-faces');
+var pg   		= require('pg');
+var httpStatus  = require('http-status-codes');
 
 var express = require('express');
 var app = express();
@@ -24,6 +26,24 @@ app.get('/cool', function(req, res) {
 	}
 	
 	res.send(output);
+});
+
+app.get('/db', function (request, response) {
+  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+  	if (err) {
+  		return response.status(httpStatus.INTERNAL_SERVER_ERROR).send(err);
+  	}
+
+    client.query('SELECT * FROM test_table', function(err, result) {
+    	done();
+    	if (err) {
+    		console.error(err); 
+    		return response.status(httpStatus.INTERNAL_SERVER_ERROR);
+    	} else {
+    		response.json({results: result.rows});
+    	}
+    });
+  });
 });
 
 app.listen(app.get('port'), function() {
